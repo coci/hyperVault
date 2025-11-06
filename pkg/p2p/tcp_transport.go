8e918c747aab7654eea72f176efc4628cfc09d43
soroush <soroush.safari1992@gmail.com>
2025-11-06 16:31:36 +0330

    Add ListenAndAccept method and connection handling to TCPTransport in pkg/p2p package

--- pkg/p2p/tcp_transport.go
+++ pkg/p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -19,4 +20,31 @@ func NewTCPTransport(listenAddress string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddress,
 	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() (net.Conn, error) {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept err %s\n", err)
+		}
+		go t.HandleConnection(conn)
+	}
+}
+
+func (t *TCPTransport) HandleConnection(conn net.Conn) {
+	fmt.Printf("new incoming connection %+v\n", conn)
 }
