c278240a7b8057a6dcc52c240765852050e203e3
soroush <soroush.safari1992@gmail.com>
2025-11-06 16:22:12 +0330

    Add basic Transport and Peer interfaces to pkg/p2p package

---
+++ pkg/p2p/transport.go
@@ -0,0 +1,10 @@
+package p2p
+
+// Peer represents the remote node
+type Peer interface {
+}
+
+// Transport that handles the connection between nodes in the network
+// this can be (TCP, UDP, WebRTC)
+type Transport interface {
+}
