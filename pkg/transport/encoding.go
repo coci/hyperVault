0900d213867a2fb878e1f988ad6ee255beca108b
soroush <soroush.safari1992@gmail.com>
2025-11-08 21:33:22 +0330

    Update Decoder interface and implementations to use RPC instead of Message

--- pkg/transport/encoding.go
+++ pkg/transport/encoding.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Decoder interface {
-	Decode(reader io.Reader, msg *Message) error
+	Decode(reader io.Reader, msg *RPC) error
 }
 
 type GOBDecoder struct{}
 
-func (dec GOBDecoder) Decode(reader io.Reader, msg *Message) error {
+func (dec GOBDecoder) Decode(reader io.Reader, msg *RPC) error {
 	return gob.NewDecoder(reader).Decode(msg)
 }
 
 type DefaultDecoder struct{}
 
-func (dec DefaultDecoder) Decode(reader io.Reader, msg *Message) error {
+func (dec DefaultDecoder) Decode(reader io.Reader, msg *RPC) error {
 	buf := make([]byte, 1024)
 
 	n, err := reader.Read(buf)
