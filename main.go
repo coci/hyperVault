58cee9f297cca25e4af4be700c8a4aa947bbc12b
soroush <soroush.safari1992@gmail.com>
2025-11-08 21:29:18 +0330

    Integrate TCPTransport into main application with default options and basic error handling

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/coci/hyperVault/pkg/transport"
+)
 
 func main() {
 	fmt.Println("we are running")
+
+	tcpOps := transport.TCPTransportOption{
+		ListenAddr:    ":3000",
+		Decoder:       transport.DefaultDecoder{},
+		HandShakeFunc: transport.NOPHandShaker,
+	}
+
+	tr := transport.NewTCPTransport(tcpOps)
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
